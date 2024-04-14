a2e7d0bebc5775a0ac0b7322a08b0536e5776f3f
Vesela Apostolova <100559743+vap0204@users.noreply.github.com>
2024-04-14 22:47:52 +0300

    working

--- hello-world-2101681070/src/app/app.component.ts
+++ hello-world-2101681070/src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hello-world-2101681070';
+
+  items =  [
+    { title: "Information", info: "Name:Vesela Apostolova,Web Developer from Bulgaria " , opened: false},
+    { title: "Hobbies", info: "Volleyball, Videogames ", opened: false},
+    { title: "Favourite Chanel ", info: "MrWebDesignerAnas", opened: false},
+    { title: "My Chanel", info: " Developing modern websites ", opened: false},
+    { title: "An example of appropriation", info: "Showing interesting and useful tutorials ", opened: false},
+
+  ];
+
+    toggle(item : any) {
+      this.items.forEach(i => {
+        if(i !== item) i.opened = false;
+    });
+      item.opened = !item.opened;
+    }
+
 }
